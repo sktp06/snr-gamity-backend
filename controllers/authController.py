2a8a16464eb02226d2dbaa7c812dcbd26cbbda01
sktp06 <kantaporn_s@cmu.ac.th>
2023-06-30 14:26:27 +0700

    fix: update auth admin

--- controllers/authController.py
+++ controllers/authController.py
@@ -5,6 +5,7 @@ from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 from models.user import User
+from models.admin import Admin
 
 db = SQLAlchemy()
 
@@ -16,11 +17,11 @@ class AuthController:
             username = request.get_json()['username']
             password = request.get_json()['password']
 
-            # Check if the username exists in the database
+            # Check if the username exists in the user table
             user = User.query.filter_by(username=username).first()
 
             if user:
-                # User exists, perform login authentication
+                # User exists, perform user login authentication
                 if bcrypt.checkpw(password.encode('utf-8'), bytes(user.password, 'utf-8')):
                     user_serialize = user.serialize
                     token = jwt.encode(
@@ -30,14 +31,28 @@ class AuthController:
                 else:
                     return jsonify({'message': 'Invalid password'}), 401
             else:
-                # User does not exist, perform sign up
-                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                # Check if the username exists in the admin table
+                admin = Admin.query.filter_by(username=username).first()
 
-                new_user = User(username=username, password=hashed_password)
-                db.session.add(new_user)
-                db.session.commit()
+                if admin:
+                    # Admin exists, perform admin login authentication
+                    if bcrypt.checkpw(password.encode('utf-8'), bytes(admin.password, 'utf-8')):
+                        admin_serialize = admin.serialize
+                        token = jwt.encode(
+                            {'admin': admin_serialize, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)},
+                            'Bearer')
+                        return jsonify({'admin': admin_serialize, 'token': token}), 200
+                    else:
+                        return jsonify({'message': 'Invalid password'}), 401
+                else:
+                    # User or admin does not exist, perform sign up as user
+                    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-                return jsonify({'message': 'User created successfully'}), 201
+                    new_user = User(username=username, password=hashed_password, game_id=None, role='user')
+                    db.session.add(new_user)
+                    db.session.commit()
+
+                    return jsonify({'message': 'User created successfully'}), 201
 
         except KeyError:
             return jsonify({'message': 'The request body requires username and password'}), 400
--- models/admin.py
+++ models/admin.py
@@ -1,18 +1,16 @@
 from models.database import db
 
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Admin(db.Model):
+    __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), nullable=False, default='user')
-    game_id = db.Column(db.Integer, nullable= True)
+    role = db.Column(db.String(50), nullable=False, default='user')  # Add the 'role' column with a default value of 'user'
 
-    def __init__(self, username, password, game_id, role):
+    def __init__(self, username, password, role):
         self.username = username
         self.password = password
-        self.game_id = game_id
         self.role = role
 
     # Convert, forward, store for readable
@@ -22,6 +20,5 @@ class User(db.Model):
             'id': self.id,
             'username': self.username,
             'password': self.password,
-            'role': self.role,
-            'game_id': self.game_id,
+            'role': self.role
         }
